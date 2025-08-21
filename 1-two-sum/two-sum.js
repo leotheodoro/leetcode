4def0c3cf8bb4e2df2d96d866e4c9cad62d5cbd8
Leonardo Theodoro <leonardo@gugale.com.br>
2025-08-20 22:25:59 -0300

    Time: 118 ms (5.48%) | Memory: 53.7 MB (92.53%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,8 +4,6 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const o = {}
-
     for(let x = 0; x < nums.length; x++) {
         for(let y = 0; y < nums.length; y++) {
             if(x !== y && nums[x] + nums[y] === target) {
