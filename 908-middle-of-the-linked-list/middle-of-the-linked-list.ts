49dd9ba94fea993f67c93d41b5e03ebe4d3fbfd2
Leonardo Theodoro <leonardo@gugale.com.br>
2025-08-21 15:43:26 -0300

    Time: 0 ms (100.00%) | Memory: 55.5 MB (42.72%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+    let ahead = head
+
+    while(ahead && ahead.next) {
+        ahead = ahead.next.next
+        head = head.next
+    }
+
+    return head
+};
