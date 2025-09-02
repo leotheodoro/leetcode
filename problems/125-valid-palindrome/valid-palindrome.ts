8a3f0c3d171eca03b84380c0e371ee29aa7109f4
Leonardo Theodoro <leonardo@gugale.com.br>
2025-09-02 17:49:08 -0300

    Time: 7 ms (36.14%) | Memory: 57.2 MB (96.13%) - LeetSync

--- problems/125-valid-palindrome/valid-palindrome.ts
+++ problems/125-valid-palindrome/valid-palindrome.ts
@@ -1,15 +1,10 @@
 function isPalindrome(s: string): boolean {
- 
-
     const textFormatted = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-    console.log(textFormatted)
 
     let left = 0
     let right = textFormatted.length - 1
 
     while(left < right) {
-        console.log('left', textFormatted[left])
-        console.log('right', textFormatted[right])
         if(textFormatted[left] !== textFormatted[right]) {
             return false
         }
