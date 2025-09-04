6cff008c37e939ff2b5b97c4b3f0535d069cf09d
Leonardo Theodoro <leonardo@gugale.com.br>
2025-09-04 09:14:43 -0300

    Time: 0 ms (100.00%) | Memory: 53.8 MB (39.36%) - LeetSync

---
+++ problems/908-middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let ahead = head
+
+    while(ahead && ahead.next) {
+        ahead = ahead.next.next
+        head = head.next
+    }
+
+    return head
+};
